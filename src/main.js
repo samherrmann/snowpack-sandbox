d8dd61a96e6c44292b5cc53834f2e0af721ee7bf
Sam Herrmann <samherrmann.github@gmail.com>
2021-07-15 09:04:07 -0400

    Get API data with axios

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+import axios from 'axios';
+
 const h1 = document.querySelector('h1');
-h1.innerHTML = 'Snowpack Sandbox';
+
+try {
+  const res = await axios.get('/api.json');
+  h1.innerHTML = res.data.message;
+} catch (err) {
+  h1.innerHTML = 'Failed to get API data.'
+}
