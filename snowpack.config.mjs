3b3cf36a9be66a5b7a75e52c23eaadd56fd3a320
Sam Herrmann <samherrmann.github@gmail.com>
2021-07-15 08:50:52 -0400

    Add snowpack.config.mjs

---
+++ snowpack.config.mjs
@@ -0,0 +1,10 @@
+// The added "@type" comment will enable TypeScript type information via VSCode, etc.
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  plugins: [],
+  root: "src",
+  buildOptions: {
+    out: "dist"
+  }
+};
