f23b8915a7346301cb23f1cdba18f16a9345f250
Sam Herrmann <samherrmann.github@gmail.com>
2021-07-15 21:53:07 -0400

    Add @snowpack/plugin-sass

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -2,7 +2,10 @@
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
-  plugins: ['@snowpack/plugin-typescript'],
+  plugins: [
+    '@snowpack/plugin-typescript',
+    '@snowpack/plugin-sass'
+  ],
   root: "src",
   buildOptions: {
     out: "dist"
